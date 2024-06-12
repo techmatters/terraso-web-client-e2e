5af4e26b094f5e7d29bc0adfb3c346592b00c27e
Jose Buitron <jose.buitron@gmail.com>
2024-06-12 12:04:24 -0500

    fix: Removed debug log

--- src/auth.js
+++ src/auth.js
@@ -1,10 +1,6 @@
 import { AUTH_TOKEN, COOKIE_DOMAIN } from './config';
 
 export const setAuthCookie = async context => {
-  console.log({
-    AUTH_TOKEN,
-    COOKIE_DOMAIN 
-  })
   await context.addCookies([
     {
       name: 'atoken',
