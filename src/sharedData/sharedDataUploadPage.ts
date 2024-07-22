29951bb3958a9bef3b694a6307d95c15cb11e012
Jose Buitron <jose.buitron@gmail.com>
2024-07-22 11:37:58 -0500

    fix: Added constant for formats list

--- src/sharedData/sharedDataUploadPage.ts
+++ src/sharedData/sharedDataUploadPage.ts
@@ -1,8 +1,26 @@
 import { Page } from '@playwright/test';
 import path from 'path';
 
-const FILE_FORMATS =
-  '*.csv, *.doc, *.docx, *.geojson, *.gpx, *.jpeg, *.jpg, *.json, *.kml, *.kmz, *.pdf, *.png, *.ppt, *.pptx, *.xls, *.xlsx, *.zip';
+const FILE_FORMATS = [
+  'csv',
+  'doc',
+  'docx',
+  'geojson',
+  'gpx',
+  'jpeg',
+  'jpg',
+  'json',
+  'kml',
+  'kmz',
+  'pdf',
+  'png',
+  'ppt',
+  'pptx',
+  'xls',
+  'xlsx',
+  'zip',
+];
+const FILE_FORMATS_TEXT = FILE_FORMATS.map(format => `*.${format}`).join(', ');
 
 export const goToGroupPage = async (page: Page, slug: string) => {
   await page.goto(`/groups/${slug}/upload`);
@@ -10,7 +28,7 @@ export const goToGroupPage = async (page: Page, slug: string) => {
 
 export const addFile = async (page: Page, url: string, name: string) => {
   const dropZone = page.getByRole('button', {
-    name: `Accepted file formats: ${FILE_FORMATS} Maximum file size: 50 MB`,
+    name: `Accepted file formats: ${FILE_FORMATS_TEXT} Maximum file size: 50 MB`,
   });
   const input = await dropZone.locator('input[type=file]');
   await input.setInputFiles(path.join(__dirname, url));
