0f564e2d487be4e6ddc25e6093a6e8e3523e0193
Jose Buitron <jose.buitron@gmail.com>
2024-07-22 11:09:24 -0500

    refactor: Moved file formats to constant

--- src/sharedData/sharedDataUploadPage.ts
+++ src/sharedData/sharedDataUploadPage.ts
@@ -1,13 +1,16 @@
 import { Page } from '@playwright/test';
 import path from 'path';
 
+const FILE_FORMATS =
+  '*.csv, *.doc, *.docx, *.geojson, *.gpx, *.jpeg, *.jpg, *.json, *.kml, *.kmz, *.pdf, *.png, *.ppt, *.pptx, *.xls, *.xlsx, *.zip';
+
 export const goToGroupPage = async (page: Page, slug: string) => {
   await page.goto(`/groups/${slug}/upload`);
 };
 
 export const addFile = async (page: Page, url: string, name: string) => {
   const dropZone = page.getByRole('button', {
-    name: 'Accepted file formats: *.csv, *.doc, *.docx, *.geojson, *.gpx, *.jpeg, *.jpg, *.json, *.kml, *.kmz, *.pdf, *.png, *.ppt, *.pptx, *.xls, *.xlsx, *.zip Maximum file size: 50 MB',
+    name: `Accepted file formats: ${FILE_FORMATS} Maximum file size: 50 MB`,
   });
   const input = await dropZone.locator('input[type=file]');
   await input.setInputFiles(path.join(__dirname, url));
