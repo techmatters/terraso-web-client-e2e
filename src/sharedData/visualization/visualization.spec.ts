8d6e33b1b39603088bd6937c2bb0d5c852db9ca9
Jose Buitron <jose.buitron@gmail.com>
2024-09-27 11:04:39 -0500

    fix: Added url check for unique map URLs

--- src/sharedData/visualization/visualization.spec.ts
+++ src/sharedData/visualization/visualization.spec.ts
@@ -186,8 +186,8 @@ const testVisualizationForm = async ({
 
   // View page
   const url = new URL(page.url());
-  const expectedPathname = `/groups/${group.slug}/map/${visualizationSlug}`;
-  expect(url.pathname).toBe(expectedPathname);
+  const expectedPattern = new RegExp(`/\/groups\/${group.slug}\/map\/[\\w-]+\/pw-map-${group.slug}/`);
+  expect(url.pathname).toMatch(expectedPattern);
   await visualizationFormPage.changeBaseMap(page, 'Satellite');
 
   // Download file
